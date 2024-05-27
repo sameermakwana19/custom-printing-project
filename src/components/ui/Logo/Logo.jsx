aff24a365fff09dd82a2b995d7c5bc9d0e161a71
sameermakwana19 <sameermakwana19@gmail.com>
2024-05-27 12:40:40 +0530

    chore: Add link to homepage in Logo component

--- src/components/ui/Logo/Logo.jsx
+++ src/components/ui/Logo/Logo.jsx
@@ -1,9 +1,12 @@
 import logo from "../../../assets/print-1-logo.svg";
+import { Link } from "react-router-dom";
 
 const Logo = () => {
   return (
     <div className="logo-container">
-      <img src={logo} className="navbar__logo" alt="Logo Image" />
+      <Link to="/">
+        <img src={logo} className="navbar__logo" alt="Logo Image" />
+      </Link>
     </div>
   );
 };
