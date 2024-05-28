5460395c6908c121c15451b85831f8fddc2e7c40
sameermakwana19 <sameermakwana19@gmail.com>
2024-05-28 12:02:36 +0530

    Revert "chore: Add link to homepage in Logo component"
    This reverts commit aff24a365fff09dd82a2b995d7c5bc9d0e161a71.

--- src/components/ui/Logo/Logo.jsx
+++ src/components/ui/Logo/Logo.jsx
@@ -1,12 +1,9 @@
 import logo from "../../../assets/print-1-logo.svg";
-import { Link } from "react-router-dom";
 
 const Logo = () => {
   return (
     <div className="logo-container">
-      <Link to="/">
-        <img src={logo} className="navbar__logo" alt="Logo Image" />
-      </Link>
+      <img src={logo} className="navbar__logo" alt="Logo Image" />
     </div>
   );
 };
