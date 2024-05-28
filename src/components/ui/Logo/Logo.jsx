033bf634a09255fac1738e272e6613814e466820
sameermakwana19 <sameermakwana19@gmail.com>
2024-05-28 12:05:49 +0530

    refactor: Add homepage link to Logo component

--- src/components/ui/Logo/Logo.jsx
+++ src/components/ui/Logo/Logo.jsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
 import logo from "../../../assets/print-1-logo.svg";
 
 const Logo = () => {
   return (
     <div className="logo-container">
-      <img src={logo} className="navbar__logo" alt="Logo Image" />
+      <Link to="/">
+        <img src={logo} className="navbar__logo" alt="Logo Image" />
+      </Link>
     </div>
   );
 };
